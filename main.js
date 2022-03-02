966c6fff00aa90098a1506370391216f5e586062
junedshaikh7 <100776718+junedshaikh7@users.noreply.github.com>
2022-03-02 18:38:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let add = require('./add');
+let subs = require('./sub');
+let divide = require('./divide');
+let multi = require('./multi');
+
+
+console.log(add(100,200));
+console.log(subs(1000,300));
+console.log(divide(100,2));
+console.log(multi(100,4));
